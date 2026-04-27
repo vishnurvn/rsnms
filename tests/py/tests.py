de69dd4afd52b9c3e5c0dae4fed04051c6f5ad4b
Vishnu Raveendran <vishnunilambur@gmail.com>
2026-04-27 14:22:17 +0530

    Added performance test cases

--- tests/py/tests.py
+++ tests/py/tests.py
@@ -7,3 +7,10 @@ def test_nms_sanity():
     confs = np.array([0.9, 0.8], dtype=np.float32)
 
     _ = rsnms.nms(bboxes=boxes, confs=confs, iou_threshold=0.5)
+
+
+def test_nms_perf(benchmark):
+    boxes = np.random.randn(1000, 4).astype(np.float32)
+    boxes = boxes.clip(0.0, 1.0)
+    confs = np.random.randn(1000).astype(np.float32)
+    benchmark(rsnms.nms, bboxes=boxes, confs=confs, iou_threshold=0.5)
